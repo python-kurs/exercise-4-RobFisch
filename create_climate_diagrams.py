514c6716d18f1262f92d6bc08585f90c4401e2ff
RobFisch <33057958+RobFisch@users.noreply.github.com>
2019-06-03 23:46:39 +0200

    Update create_climate_diagrams.py
    in_progress

--- create_climate_diagrams.py
+++ create_climate_diagrams.py
@@ -1,23 +1,44 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
+from pathlib import Path
+
+base = Path("D:/Github/exercise-4-RobFisch/")
+data = base / "data/"
+output = base / "output/"
+
+output.mkdir (parents = True, exist_ok = True)
 
 # Import both data tables into python using pandas. Set the index column to "MESS_DATUM" and parse the column values as dates. [1P]
-garmisch  = 
-zugspitze = 
+garmisch = pd.read_csv(data / "produkt_klima_tag_20171010_20190412_01550.txt", sep = ";", index_col="MESS_DATUM", parse_dates=["MESS_DATUM"], na_values=-999.0)
+
+zugspitze = pd.read_csv(data / "produkt_klima_tag_20171010_20190412_05792.txt", sep = ";", index_col="MESS_DATUM", parse_dates=["MESS_DATUM"], na_values=-999.0)
+
+zugspitze.head()
+garmisch.head()
 
 # Clip the tables to the year 2018: [1P]
-garmisch  = 
-zugspitze = 
+
+garmisch  = garmisch.loc["2018"]
+zugspitze = zugspitze.loc["2018"]
+
 
 # Resample the temperature data to monthly averages (" TMK") and store them in simple lists: [1P]
-garmisch_agg  = 
-zugspitze_agg = 
+garmisch_agg  = list(garmisch.resample("1M").mean()[" TMK"])
+zugspitze_agg = list(zugspitze.resample("1M").mean()[" TMK"])
 
 # Define a plotting function that draws a simple climate diagram
 # Add the arguments as mentioned in the docstring below [1P]
 # Set the default temperature range from -15°C to 20°C and the precipitation range from 0mm to 370mm [1P]
-def create_climate_diagram(...):
+def create_climate_diagram(df,
+                           temp_col,
+                           prec_col,
+                           title,
+                           filename,
+                           temp_min=-15,
+                           temp_max=20,
+                           prec_min=0,
+                           prec_max=370):
     """
     Draw a climate diagram.
     
@@ -47,7 +68,6 @@ def create_climate_diagram(...):
     The figure
     
     """
-
     fig = plt.figure(figsize=(10,8))
     plt.rcParams['font.size'] = 16
 
